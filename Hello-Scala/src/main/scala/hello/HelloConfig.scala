36c0a36c69552c60b684b672b2fef1afaeae01e7
waykar-prashant <waykar-prashant@users.noreply.github.com>
2015-02-11 19:16:42 -0800

    added import

--- Hello-Scala/src/main/scala/hello/HelloConfig.scala
+++ Hello-Scala/src/main/scala/hello/HelloConfig.scala
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.{Configuration, ComponentScan}
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot;
 import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestMethod; 
 import org.springframework.web.bind.annotation.ResponseBody; 
 import org.springframework.stereotype.Controller; 
 
